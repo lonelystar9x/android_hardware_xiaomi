9f6b7499860ceae7d7cec6beae26f406a4e8a2ee
Ghosuto <clash.raja10@gmail.com>
2026-01-04 14:10:39 +0000

    dolby: Implement haptic feedback
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- dolby/src/org/lunaris/dolby/ui/components/EnhancedBottomNav.kt
+++ dolby/src/org/lunaris/dolby/ui/components/EnhancedBottomNav.kt
@@ -24,6 +24,8 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import org.lunaris.dolby.utils.*
 
 @Composable
 fun EnhancedBottomNavigationBar(
@@ -78,7 +80,9 @@ private fun EnhancedNavItem(
     isEqualizer: Boolean = false
 ) {
     val interactionSource = remember { MutableInteractionSource() }
-    
+    val haptic = rememberHapticFeedback()
+    val scope = rememberCoroutineScope()
+
     var isBouncing by remember { mutableStateOf(false) }
     val bounceScale by animateFloatAsState(
         targetValue = if (isBouncing) 1.3f else 1f,
@@ -124,6 +128,9 @@ private fun EnhancedNavItem(
                 interactionSource = interactionSource,
                 indication = null
             ) {
+                scope.launch {
+                    haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.HEAVY_CLICK)
+                }
                 isBouncing = true
                 onClick()
             },
